11d4470f738b00c2250fa1eefe3c235144409ed3
Cris Mantovani <crismantovani87@gmail.com>
2023-06-28 18:59:21 -0300

    build: :alien: ADD GoogleOAuth API & AXIOS
    ADD GoogleOAuth API | Instala AXIOS. Necessário rodar npm install

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId="307505456696-6l1r1u1if1bnnrg02knt0b090le90pgj.apps.googleusercontent.com">
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </GoogleOAuthProvider>
 );
--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,12 +1,48 @@
 import "./Register.css";
 
 import { Link } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+import { useGoogleLogin, googleLogout } from "@react-oauth/google";
+import axios from "axios";
 
 import Logo from "../../assets/img/logo.svg";
 import Google from "../../assets/img/google.svg";
 import Card from "../../components/Card/Card";
 
+const apiKey = import.meta.env.VITE_GOOGLE_OAUTH_CLIENT_ID;
+
 const Register = () => {
+  const [user, setUser] = useState([]);
+  const [profile, setProfile] = useState([]);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log("Login Failed:", error),
+  });
+
+  useEffect(() => {
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
+    }
+  }, [user]);
+
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+  };
   return (
     <div className="register">
       <Card>
@@ -16,7 +52,7 @@ const Register = () => {
           className="register__logo"
         />
 
-        <button type="submit" className="btn btn-secondary">
+        <button onClick={() => login()} className="btn btn-secondary">
           <img src={Google} alt="Logo do Google" />
           Entre com o Google
         </button>
